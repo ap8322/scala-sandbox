55bf6eee6933d20caa17e81f2f9f0305a49a16bd
ap8322 <yuki.haneda@bizreach.co.jp>
2016-10-17 09:11:27 +0900

    can done

--- src/main/scala/tptest.scala
+++ src/main/scala/tptest.scala
@@ -8,14 +8,12 @@ trait Done extends Can {
 class Person extends Can
 class Animal extends Done
 
-class SwimmyList[U >: Can <: Can](lists: U*) {
+class SwimmyList[+U >: Done <: Can](lists: U*) {
   val head = lists.head
   val tail = lists.tail
 
-  def add(newElem: U): SwimmyList[U] =
+  def add[U <: T](newElem: T): SwimmyList[U] =
     new SwimmyList(newElem :: lists.toList: _*)
 
   def sayAll() = lists.foreach(_.sayReason())
 }
-
-class test[U >: Can <: Can](test: U*) extends SwimmyList[U]
