baf3036304174ea66cba85c6d3278d6067375d7f
ap8322 <yuki.haneda@bizreach.co.jp>
2016-11-20 20:49:10 +0900

    fix

---
+++ src/main/scala/_31/Reader.scala
@@ -0,0 +1,7 @@
+import java.io._
+
+class Reader(fname: String) {
+  private val in = new BufferedReader(new FileReader(fname))
+  @throws(classOf[IOException])
+  def read() = in.read()
+}
--- src/main/scala/parser/ParserJSON.scala
+++ src/main/scala/parser/ParserJSON.scala
@@ -1,8 +1,8 @@
-import java.io.FileReader
-
-object ParserJSON extends JSON {
-  def main(args: Array[String]) {
-    val reader = new FileReader(args(0))
-    println(parseAll(value,reader))
-  }
-}
+//import java.io.FileReader
+//
+//object ParserJSON extends JSON {
+//  def main(args: Array[String]) {
+//    val reader = new FileReader(args(0))
+//    println(parseAll(value,reader))
+//  }
+//}
